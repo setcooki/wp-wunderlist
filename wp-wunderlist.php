9ebfed1366466438076aef5f87beeaee717ade93
Frank Mueller <set@cooki.me>
2015-04-24 09:12:30 +0200

    first commit

---
+++ wp-wunderlist.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Plugin Name: WP Wunderlist
+Plugin URI: http://set.cooki.me
+Description: Wunderlist plugin
+Version: 0.0.1
+Author: setcooki
+Author URI: http://set.cooki.me/me
+License: GPLv2 or later
+Text Domain: wunderlist
+*/
